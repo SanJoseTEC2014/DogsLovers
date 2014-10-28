9e8aa903048aa4b5942eaef2cdbf2600f41accd2
lizcha95 <lizchavca@gmail.com>
2014-10-28 17:29:49 -0600

    Creación Clases Calificación y Organización

--- PrototiposDL/src/dogslovers/modelo/Calificacion.java
+++ PrototiposDL/src/dogslovers/modelo/Calificacion.java
@@ -2,6 +2,25 @@ package dogslovers.modelo;
 
 public class Calificacion {
 	private Integer estrellas;
-	private
+	private String mensaje;
+	private String nicknameCalificante;
 
+	public Integer getEstrellas() {
+		return estrellas;
+	}
+	public void setEstrellas(Integer estrellas) {
+		this.estrellas = estrellas;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	public Calificacion(String pNicknameCalificante, Integer pEstrellas, String pMensaje) {
+		nicknameCalificante = pNicknameCalificante;
+		estrellas = pEstrellas;
+		mensaje = pMensaje;
+	}
 }
---
+++ PrototiposDL/src/dogslovers/modelo/Organizacion.java
@@ -0,0 +1,35 @@
+package dogslovers.modelo;
+
+public class Organizacion {
+	private Integer id;
+	private String nombre;
+	private String direccion;
+	private Integer numeroContacto;
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Integer getNumeroContacto() {
+		return numeroContacto;
+	}
+	public void setNumeroContacto(Integer numeroContacto) {
+		this.numeroContacto = numeroContacto;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public Organizacion(String pNombre, Integer pID, Integer pNumeroContacto, String pDireccion) {
+		nombre = pNombre;
+		id = pID;
+		numeroContacto = pNumeroContacto;
+		direccion = pDireccion;
+	}
+
+}
