1790cda5dbb45dd7e99b29ff1924c605a8058874
Andrés Peña <lapc20081996@gmail.com>
2014-11-10 23:51:10 -0600

    final

--- PrototiposDL/src/dogslovers/control/CasosPrueba.java
+++ PrototiposDL/src/dogslovers/control/CasosPrueba.java
@@ -37,7 +37,7 @@ public class CasosPrueba {
 				Principal.blanca.add(porRegistrar);
 			}
 			JOptionPane.showMessageDialog(null,
-					"Casos de prueba cargados satisfactoriamente.");
+					"Casos de prueba de usuarios cargados satisfactoriamente.");
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null,
 				e.getMessage(),
@@ -73,27 +73,50 @@ public class CasosPrueba {
 				 * 
 				 * registro.get(15) Numero Chip
 				 */
-				Mascota porRegistrar = new Mascota(registro.get(1), registro.get(2),
-						registro.get(3), Integer.parseInt(registro.get(4)));
-				switch(registro.get(11)){
+				/**
+				 * registro.get(0) Nombre
+				 * registro.get(1) Especie
+				 * registro.get(2) Raza
+				 * registro.get(3) Recompensa
+				 * 
+				 * SUCESO:
+				 * registro.get(4) Nickname Registrante
+				 * registro.get(5) Descripción
+				 * registro.get(6) Lugar
+				 * 
+				 * registro.get(7) Sexo
+				 * registro.get(8) Color
+				 * registro.get(9) Tamaño
+				 * registro.get(10) Estado Inicial
+				 * 
+				 * Características
+				 * registro.get(11) Castrada
+				 * registro.get(12) Vacunada
+				 * registro.get(13) Desparacitada
+				 * 
+				 * registro.get(14) Numero Chip
+				 */
+				Mascota porRegistrar = new Mascota(registro.get(0), registro.get(1),
+						registro.get(2), Integer.parseInt(registro.get(3)));
+				switch(registro.get(10)){
 				case "PERDIDO":
-					porRegistrar.addPerdida(new Suceso(registro.get(5), registro.get(7), registro.get(6)));
+					porRegistrar.addPerdida(new Suceso(registro.get(4), registro.get(6), registro.get(5)));
 					break;
 				case "ENCONTRADO":
-					porRegistrar.addEncuentro(new Suceso(registro.get(5), registro.get(7), registro.get(6)));
+					porRegistrar.addEncuentro(new Suceso(registro.get(4), registro.get(6), registro.get(5)));
 					break;
 				case "ENADOPCION":
-					porRegistrar.addAdoptable(new Suceso(registro.get(5), registro.get(7), registro.get(6)));
+					porRegistrar.addAdoptable(new Suceso(registro.get(4), registro.get(6), registro.get(5)));
 					break;
 				}
-				porRegistrar.setSexo(registro.get(8));
-				porRegistrar.setColor(registro.get(9));
-				porRegistrar.setTamanio(registro.get(10));
-				porRegistrar.setCastrada(registro.get(12).equals("SI") ? true : false);
-				porRegistrar.setVacunada(registro.get(13).equals("SI") ? true : false);
-				porRegistrar.setDesparacitada(registro.get(14).equals("SI") ? true : false);
-				porRegistrar.setNumeroChip(registro.get(15));
-				switch(registro.get(11)){
+				porRegistrar.setSexo(registro.get(7));
+				porRegistrar.setColor(registro.get(8));
+				porRegistrar.setTamanio(registro.get(9));
+				porRegistrar.setCastrada(registro.get(11).equals("SI") ? true : false);
+				porRegistrar.setVacunada(registro.get(12).equals("SI") ? true : false);
+				porRegistrar.setDesparacitada(registro.get(13).equals("SI") ? true : false);
+				porRegistrar.setNumeroChip(registro.get(14));
+				switch(registro.get(10)){
 				case "PERDIDO":
 					Principal.perdidas.add(porRegistrar);
 					break;
@@ -105,6 +128,8 @@ public class CasosPrueba {
 					break;
 				}
 			}
+			JOptionPane.showMessageDialog(null,
+					"Casos de prueba de mascotas cargados satisfactoriamente.");
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null,
 				e.getMessage(),
